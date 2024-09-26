5bd7c56e63bfa786ad2bbb76aa300c3bf73b4714
Aditya Raikar <raikaraditya22@gmail.com>
2024-09-26 20:14:33 +0200

    fixed index issue

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ require("./config")(app);
 
 // 👇 Start handling routes here
 const indexRoutes = require("./routes/index.routes");
-app.use("/api", indexRoutes);
+app.use("/", indexRoutes);
 
 const authRouter = require("./routes/auth.routes");     
 app.use("/auth", authRouter); 
